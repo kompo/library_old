9e4f722a2e5fa047e2524eb9b145f64de5701a45
Bass El Hachem <basselhachem@kompo.io>
2020-05-20 21:10:17 -0400

    reinstallation

--- src/KompoLibraryServiceProvider.php
+++ src/KompoLibraryServiceProvider.php
@@ -35,7 +35,6 @@ class KompoLibraryServiceProvider extends ServiceProvider
         \Gate::before(function ($user, $ability) {
             return $user->hasRole('super-admin') ? true : null;
         });
-        
 
         app('router')->aliasMiddleware('role', RoleMiddleware::class);
         app('router')->aliasMiddleware('permission', PermissionMiddleware::class);
